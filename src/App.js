2ba1a67d8866b48b8b14b6828970ab3812d9c5c3
Luis Castillo <mr.luiscastillo@gmail.com>
2021-11-28 23:22:39 -0500

    add handle change to SignUp and import SignUp Component to app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import SignUp from './components/SignUp'
 
 class App extends Component {
 
@@ -9,7 +10,7 @@ class App extends Component {
 render(){
     return (
             <div className="App">
-
+                <SignUp />
             </div>
         );
     }
