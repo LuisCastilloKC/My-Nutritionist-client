d0cdea5f6c8ac86aaf3dcfc37aa70ceb04761890
Luis Castillo <mr.luiscastillo@gmail.com>
2021-12-25 23:31:02 -0500

    change: function component to class and onChange username and password

--- src/components/LogInForm.jsx
+++ src/components/LogInForm.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const LogInForm = () => {
+class LogInForm extends Component {
 
-    return(
-        <div> 
-            <form>
-            <h1> Log In</h1>
-                <label>Log In</label>
-                <input type="text" name="Login" />
-                <label>Password</label>
-                <input type="password" name="password" />
-                <input type="submit" value="Password" />
+   handleSubmit = (e) =>{
+    e.preventDefault();
+
+    
+    }
+
+render(){ 
+        return(
+            <div> 
+                <form onSubmit={this.handleSubmit}>
+                <h1>Login</h1>
+                    <label>Username</label>
+                    <input type="text" name="username" placeholder="Username" onChange={e => this.username = e.target.value} />
+                    <label>Password</label>
+                    <input type="password" name="password" placeholder="Password" onChange={e => this.password = e.target.value} />
+                    <input type="submit" value="Password" />
                 
-            </form>
-        </div>
-    );
-};
+                </form>
+            </div>
+        );
+    };
+}
 
 export default LogInForm;
