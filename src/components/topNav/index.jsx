18816f93c7cb8b3443a590b0231db25ad171b85c
Luis Castillo <mr.luiscastillo@gmail.com>
2022-02-07 23:51:17 -0500

    add topNav

---
+++ src/components/topNav/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TopNav = () =>{
+
+    return(
+        <div className='topnav'>
+            <div className='topnavWrapper'>
+                <div className='topLeft'>My Nutritionist</div>
+                <div className='topRight'>UserProfile</div>
+            </div>
+        </div>
+    );
+};
+
+export default TopNav;
