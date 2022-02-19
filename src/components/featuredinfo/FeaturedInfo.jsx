85faabf284f6f00d0961dea9aaa0c670e459f3a4
Luis Castillo <mr.luiscastillo@gmail.com>
2022-02-18 23:43:27 -0500

    modify featured card

--- src/components/featuredinfo/FeaturedInfo.jsx
+++ src/components/featuredinfo/FeaturedInfo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Featured, FeautedItem } from './featuredInfoElements';
+import { Featured, FeautedItem, AppointmentProfilePic, ProfileName } from './featuredInfoElements';
+import  { MoreVert }  from '@material-ui/icons';
 
 
 
@@ -8,7 +9,9 @@ const FeaturedInfo = () => {
     return(
         <Featured>
             <FeautedItem>
-                <span className='feautedTitle'>Title Here</span>
+                <AppointmentProfilePic src="https://cdn1.iconfinder.com/data/icons/website-internet/48/website_-_female_user-512.png"></AppointmentProfilePic>
+                <ProfileName>Title Here</ProfileName>
+                <span className='feautedTitle'><MoreVert /></span>
                 <div className='featuredAppointmentContainer'>
                     <span className='featureAppointmentTotal'>5 appointment </span>
                     <span className='featureAppointmentPending'>5 Pendind </span>
--- src/components/featuredinfo/featuredInfoElements.js
+++ src/components/featuredinfo/featuredInfoElements.js
@@ -8,10 +8,20 @@ export const Featured = styled.div`
 export const FeautedItem = styled.div`
     flex: 1;
     margin: 0px 20px;
-    padding: 30px;
+    padding: 15px;
     border-radius: 10px;
-    cursor: pointer;
+    
     
     -webkit-box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
     box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
+`
+
+export const AppointmentProfilePic = styled.img`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;    
+`
+
+export const ProfileName = styled.span`
+    
 `
