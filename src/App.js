703d2ddc9eeb3b0f23722474d5dc9acac2765133
Luis Castillo <mr.luiscastillo@gmail.com>
2022-02-25 23:46:04 -0500

    initialize Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 //import { Routes, Route } from 'react-router-dom'
 import React from 'react';
+import { Routes, Route } from 'react-router-dom'
 import GlobalStyles from './styles/Global';
 import TopNav from './components/topNav/Topnav';
 import Sidenav from './components/sideNav/Sidenav';
 import { SideNavContainer } from './appElements'
 import Home from './pages/home/Home';
+import Appointment from './components/appointments/Appointment';
 
 
 const App = () => {
@@ -17,6 +19,11 @@ const App = () => {
                 <Sidenav />
                 <Home />              
             </SideNavContainer>
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="appointments" element={<Appointment />} />
+            </Routes>
                
         </div>
         );
