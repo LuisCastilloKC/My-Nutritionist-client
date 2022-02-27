5db423e818b3800bf7fba5ff18bd9d1e006fd259
Luis Castillo <mr.luiscastillo@gmail.com>
2022-02-26 23:59:14 -0500

    fix appointment route

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import GlobalStyles from './styles/Global';
 import TopNav from './components/topNav/Topnav';
 import Sidenav from './components/sideNav/Sidenav';
 import { SideNavContainer } from './appElements'
-import AppointmentRoute from './routes/appointments/AppointmentRoute';
+import AppointmentRoute from './routes/appointments/AppointmentRoute'
 import LogIn from './routes/login/LogIn';
 import SignUp from './routes/signup/SignUp'
 import HomeRoute from './routes/home/HomeRoute';
@@ -21,11 +21,9 @@ const App = () => {
                 <Sidenav />              
             <Routes>
                 <Route path="/" element={<HomeRoute />} />
-                <Route path="appointments" element={<AppointmentRoute />} />
-                <Route path="clients" element={<AppointmentRoute />} />
+                <Route path="/appointment" element={<AppointmentRoute />} />
                 <Route path="login" element={<LogIn />} />
                 <Route path="signup" element={<SignUp />} />
-                <Route path="mealplan" element={<AppointmentRoute />} />
             </Routes>
             </SideNavContainer>
 
