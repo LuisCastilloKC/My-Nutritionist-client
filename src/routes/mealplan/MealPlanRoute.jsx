7ddc7d6ee5b142c506b8416247b110c9d02e0361
Luis Castillo <mr.luiscastillo@gmail.com>
2022-02-27 23:50:24 -0500

    mealplan route component

---
+++ src/routes/mealplan/MealPlanRoute.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { CenterBox } from '../../styles/centerBoxElements';
+
+const MealPlanRoute = () => {
+
+    return(
+        <CenterBox>
+            Meal Plan Here
+        </CenterBox>
+    );
+};
+
+export default MealPlanRoute
