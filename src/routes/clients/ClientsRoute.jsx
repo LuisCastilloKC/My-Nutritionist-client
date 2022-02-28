67aef700f9478fdcc9a4bef61b01033e02fcc0a2
Luis Castillo <mr.luiscastillo@gmail.com>
2022-02-27 23:58:27 -0500

    clientsRoute

---
+++ src/routes/clients/ClientsRoute.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { CenterBox } from '../../styles/centerBoxElements';
+
+const ClientsRoute = () =>{
+
+    return(
+        <CenterBox>
+
+        </CenterBox>      
+    );
+};
+
+export default ClientsRoute
