5dd8eb811c9fce176926e57581276fb5dea4b2a2
Luis Castillo <mr.luiscastillo@gmail.com>
2022-04-23 23:59:02 -0400

    initialize email and password state

--- src/components/logIn/LogIn.jsx
+++ src/components/logIn/LogIn.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React,{ useState } from 'react';
 
 const LogIn = () =>{
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return(
-        <div>
-            <h1> LogIn Here</h1>
-        </div>
+      <div>
+          <form>
+
+          </form>
+      </div>
     );
 };
 
