fc8c2c2e9d17d5909e53f963fd7ca607c24974fa
Luis Castillo <mr.luiscastillo@gmail.com>
2022-04-26 01:36:53 -0400

    add form validation and onSubmit function

--- src/components/logIn/LogIn.jsx
+++ src/components/logIn/LogIn.jsx
@@ -4,14 +4,22 @@ const LogIn = () =>{
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const validationForm = () => {
+      return email.length > 0 && password.length > 0
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return(
       <div className='login'>
-          <form>
+          <form onSubmit={handleSubmit}>
             <label>Email</label>
-            <input type="email" value={email} onChange={(e)=> setEmail(e.target.value)} />
+            <input type="email" value={email} onChange={(e)=> setEmail(e.target.value)} placeholder="Your email" />
             <label>Password</label>
-            <input type="password" value={(password)} onChange={(e)=> setPassword(e.target.value)} />
-            <button type="submit">Login</button>
+            <input type="password" value={password} onChange={(e)=> setPassword(e.target.value)} placeholder="Your password" />
+            <button type="submit" disabled={!validationForm()}>Login</button>
           </form>
       </div>
     );
