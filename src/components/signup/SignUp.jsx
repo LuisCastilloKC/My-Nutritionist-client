5568a706ae31fa98efea255ada59e41cd12f859e
Luis Castillo <mr.luiscastillo@gmail.com>
2022-04-27 23:21:54 -0400

    add sign up form field

--- src/components/signup/SignUp.jsx
+++ src/components/signup/SignUp.jsx
@@ -13,7 +13,7 @@ const SignUp = () => {
     const handleInputChange = (e)=>{
         const [name, value] = e.target
 
-        setInputsValues({
+        setInputsValue({
             ...inputsValue,
             [name]: value
         })
@@ -26,6 +26,13 @@ const SignUp = () => {
                 <h2>Sign Up</h2>
                 <label>First Name:</label>
                 <input type="text" value={inputsValue.firstName} onChange={handleInputChange} name="firstName" />
+                <label>Last Name:</label>
+                <input type="text" value={inputsValue.lastName} onChange={handleInputChange} name="lastName" />
+                <label>Email:</label>
+                <input type="email" value={inputsValue.email} onChange={handleInputChange} name="email" />
+                <label>Password:</label>
+                <input type="password" value={inputsValue.password} onChange={handleInputChange} name="password" />
+                <button type="submit">SingUp</button>
             </form>
         </div>
     );
