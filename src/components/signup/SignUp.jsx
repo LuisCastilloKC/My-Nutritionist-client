f9fd8c6cb92c272a2fd9fdac782b4a38c19478e5
Luis Castillo <mr.luiscastillo@gmail.com>
2022-04-28 23:49:55 -0400

    add confirm password

--- src/components/signup/SignUp.jsx
+++ src/components/signup/SignUp.jsx
@@ -9,6 +9,7 @@ const SignUp = () => {
     }
 
     const [inputsValue, setInputsValue] = useState(initialValues)
+    const [confirmPasswordValue, setConfirmPasswordValue] = useState('')
 
     const handleInputChange = (e)=>{
         const [name, value] = e.target
@@ -32,13 +33,40 @@ const SignUp = () => {
             <form onSubmit={handleSubmit}>
                 <h2>Sign Up</h2>
                 <label>First Name:</label>
-                <input type="text" value={inputsValue.firstName} onChange={handleInputChange} name="firstName" placeholder="First Name" />
+                <input 
+                    type="text" 
+                    value={inputsValue.firstName} 
+                    onChange={handleInputChange} 
+                    name="firstName" 
+                    placeholder="First Name" />
                 <label>Last Name:</label>
-                <input type="text" value={inputsValue.lastName} onChange={handleInputChange} name="lastName"  placeholder="Last Name" />
+                <input 
+                    type="text" 
+                    value={inputsValue.lastName} 
+                    onChange={handleInputChange} 
+                    name="lastName"  
+                    placeholder="Last Name" />
                 <label>Email:</label>
-                <input type="email" value={inputsValue.email} onChange={handleInputChange} name="email" placeholder="Email" />
+                <input 
+                    type="email" 
+                    value={inputsValue.email} 
+                    onChange={handleInputChange} 
+                    name="email" 
+                    placeholder="Email" />
                 <label>Password:</label>
-                <input type="password" value={inputsValue.password} onChange={handleInputChange} name="password" placeholder="Password" />
+                <input 
+                    type="password" 
+                    value={inputsValue.password} 
+                    onChange={handleInputChange} 
+                    name="password" 
+                    placeholder="Password" />
+                <label>Confirm Password</label>
+                <input 
+                    type={password}
+                    value={confirmPasswordValue}
+                    onChange={handleChange}
+                    name="password"
+                    placeholder="password" />
                 <button type="submit">SingUp</button>
             </form>
         </div>
